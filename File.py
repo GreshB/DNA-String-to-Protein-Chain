e204ba1fd4f677542a773a0498a12d7706d56b41
Gresh Basic <100721569+GreshB@users.noreply.github.com>
2022-03-06 18:55:32 -0600

    Update File.py
    All updates since the initial posting of this program:
    - removed all if statements used to convert the DNA sequence, RNA sequence, and codons, and replaced them with dictionaries
    - added the ability for two chains to be outputted
    - added aesthetic formating to the outputted chain(s)
    - added descriptions to each function
    - added comments to if statements and loops to describe what their purpose is
    - formatted the code to be read more easily

--- File.py
+++ File.py
@@ -15,6 +15,7 @@ def transcription1(dna):
         'g' : 'c',
     }
 
+    # creates a string that converts the DNA sequenence
     for i in range(len(dna)):
         trans1.append(transcription_map[lcdna[i]])
     return string.join(trans1)
@@ -36,11 +37,13 @@ def transcription2(dna):
         'g' : 'c',
     }
 
+    # creates a string that converted the DNA sequence into RNA
     for i in range(len(dna)):
         trans2.append(transcription_map[lcdna[i]])
 
     trans2_string = string.join(trans2)
 
+    # makes sure the length of the string is divisible by 3 due to later neccessities
     while len(trans2_string) % 3 != 0:
         trans2_string = trans2_string[:-1]
 
@@ -51,7 +54,7 @@ def frame_for_met(dna):
     Purpose: To frame the RNA sequence created from the DNA sequence in such a way
              that if "aug" is in the string, then a created list that has organized
              the RNA sequence into codons, ensures that "aug" will be one of its elements
-             will be returned. (Example: "caug" would become ["cau","g"] without framing, 
+             will be returned. (Example: "caug" would become ["cau","g"] without framing,
              so this function will instead alter it to ["c","aug"])
     Parameter(s): DNA: the inputted DNA sequence
     Return Values: dna_split_list: The RNA sequence in list form in which "aug" is an element,
@@ -71,6 +74,9 @@ def frame_for_met(dna):
     dna_split_list3 = [dna_list[i:i+n] for i in range(2, len(dna_list), n)]
     met_first = []
 
+    # rearranging the codon list to find an element "aug", if it cannot be found,
+    # a message informing the user that a protein will not be produced
+    # as it is not possbile to make "aug" the first codon in the list
     if "aug" not in dna_split_list:
         if "aug" not in dna_split_list2:
             if "aug" not in dna_split_list3:
@@ -90,16 +96,20 @@ def find_met(dna):
     Parameter(s): DNA: the inputted DNA sequence
     Return Values: dna_split_list: a list consisting of RNA codons that contain "aug"
                    "None": returned if "aug" was not in the list
+                   met_first: a list in which "aug" is the first element
     '''
-    
+
     n = 3
     dna_list = transcription2(dna)
     dna_split_list = frame_for_met(dna)
     met_first = []
 
+    # checking to make sure "aug" was in dna_split_list
     if dna_split_list == None:
         return "None"
 
+    # if the first codon in the codon list is not "aug", it will attempt to produce
+    # a list in which it is
     if dna_split_list[0] != "aug":
         for i in range(1, len(dna_split_list)):
             if dna_split_list[i] == "aug":
@@ -109,6 +119,8 @@ def find_met(dna):
     else:
         return dna_split_list
 
+    # a message informing the user that a protein will not be produced
+    # as it is not possbile to make "aug" the first codon in the list
     if met_first == []:
         print(dna_split_list)
         print("\n*******************************************")
@@ -199,18 +211,24 @@ def dna_to_amino_acid_chain(dna):
     dashlist2 = []
     dna_split_list = find_met(dna)
 
+    # checking to make sure "aug" was in dna_split_list
     if dna_split_list == "None":
         return "Please try another DNA sequence"
 
+    # checking to see if the last element contains 3 nucleotides, if not,
+    # it will be deleted, since it not only wouldn't contribute to the chain
+    # it wouldn't be a key in the rna_combos dictionary
     if len(dna_split_list[-1]) % 3 != 0:
         dna_split_list = dna_split_list[:-1]
 
+    # checking to see if a codon is not a STOP codon
     for i in range(0, len(dna_split_list)):
         if rna_combos[dna_split_list[i]] != "STOP":
             trans3.append(rna_combos[dna_split_list[i]])
         else:
             break
 
+    # formating the chain to have dashes between the amino acids
     for i in range(0, len(trans3)):
         if i != len(trans3) - 1:
             dashlist.append(trans3[i])
@@ -218,16 +236,21 @@ def dna_to_amino_acid_chain(dna):
         else:
             dashlist.append(trans3[i])
 
+    # checking to see if there is another possible chain in dna_split_list,
+    # and if there is, creating a new list to produce it
     for i in range(len(trans3)+1, len(dna_split_list)):
         if dna_split_list[i] == "aug":
             trans4.append("Met")
             for j in range(i+1, len(dna_split_list)):
                 trans4.append(rna_combos[dna_split_list[j]])
 
+    # checking to see if there is a STOP codon in the second list, and if so,
+    # deleted it and everything afterwards
     if "STOP" in trans4:
         stop = trans4.index("STOP")
         del trans4[stop:]
 
+    # formating the second chain to have dashes between the amino acids
     for i in range(0, len(trans4)):
         if i != len(trans4) - 1:
             dashlist2.append(trans4[i])
@@ -235,11 +258,14 @@ def dna_to_amino_acid_chain(dna):
         else:
             dashlist2.append(trans4[i])
 
+    # checking to see if there was a second chain produced, and if so
+    # printing an additional line for it
     if len(dashlist2) != 0:
         print("First Chain:","".join(dashlist))
         print("Second Chain:","".join(dashlist2))
     else:
         print("Chain:","".join(dashlist))
+
 
 
 if __name__ == '__main__':
