0f20ef55fd8e878a1ac1aefa3f62fe432c910a5d
Nate Bowen <nate@natebowen.com>
2022-04-03 19:52:46 -0700

    Adding some tests for frame_for_met

---
+++ tests.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from File import frame_for_met, transcription2
+
+
+class DNAStringTest(TestCase):
+
+    def test_frame_for_met(self):
+        self.assertEqual(
+            ['aug', 'acu', 'gua', 'cca', 'gga'],
+            frame_for_met("atgactgtaccagga")
+        )
+        self.assertEqual(
+            ['aug', 'acu', 'gua', 'cca', 'gg'],
+            frame_for_met("gatgactgtaccagga")
+        )
+        self.assertEqual(
+            ['aug', 'acu', 'gua', 'cca', 'g'],
+            frame_for_met("tgatgactgtaccagt")
+        )
+        self.assertEqual(
+            ['ucg', 'aug', 'acu', 'gua', 'cca', 'gga'],
+            frame_for_met("tcgatgactgtaccaggatt")
+        )
